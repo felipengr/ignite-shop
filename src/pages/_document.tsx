e3aa5d2e1a98b074fc1114fd7905e9ebe87a363f
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-03-02 20:32:33 -0300

    Configurando Stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '../styles';
 
 export default function Document() {
     return (
@@ -8,6 +9,7 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link rel="icon" href="fav.svg" />
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" /> 
+                <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
             </Head>
             <body>
                 <Main />
