36b7ba7780903d8ee06d4824d11e79180eaaeeee
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-03-02 22:10:54 -0300

    Criando Carrossel com Keen Slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import { useKeenSlider } from 'keen-slider/react'
 import { HomeContainer, Product } from "../styles/pages/home";
+import 'keen-slider/keen-slider.min.css'
 
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
@@ -8,17 +10,38 @@ import camiseta4 from '../assets/camisetas/4.png'
 import camiseta5 from '../assets/camisetas/5.png'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2.5,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta Legal</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta Maneira</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta Maneira</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta4} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta Maneira</strong>
           <span>R$ 79,90</span>
