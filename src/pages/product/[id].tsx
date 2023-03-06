27dcc0f4e2c7b31e0b985fa5d988c29c92750e2f
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-03-06 18:52:37 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,27 @@
 import { useRouter } from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>Produto: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Aut atque dolorum quasi culpa modi numquam voluptas libero
+                    velit quo quis rerum placeat enim dolores impedit, 
+                    explicabo quos adipisci qui? Accusantium.
+                </p>
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
