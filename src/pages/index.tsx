43e48cf7605fcd74503408c08429c641617c8c8a
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-03-06 18:33:38 -0300

    Navegação via Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from "next";
 import Image from "next/image";
+import Link from 'next/link'
 import Stripe from "stripe";
 import { useKeenSlider } from 'keen-slider/react'
 import { HomeContainer, Product } from "../styles/pages/home";
@@ -27,13 +28,15 @@ export default function Home({products}: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
+          <Link key={product.id} href={`/product/${product.id}`}>
+            <Product className="keen-slider__slide">
             <Image src={product.imageUrl} width={520} height={480} alt="" />
             <footer>
               <strong>{product.name}</strong>
               <span>{product.price}</span>
             </footer>
           </Product>
+          </Link>
         )
       })}
     </HomeContainer>
