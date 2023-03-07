b00e57ca67e62b25d5dd828559f654b405e69702
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-03-07 16:12:49 -0300

    Estrutura de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Product() {
     return (
-        <h1>Success</h1>
+        <SuccessContainer>
+            <h1>Compre efetuada!</h1>
+            <ImageContainer>
+
+            </ImageContainer>
+            <p>
+               Uhuul <strong>Felipe Nogueira</strong>, sua
+               <strong> Camiseta Beyond the Limits</strong> já está a caminho 
+               da sua casa.
+            </p>
+            <Link href="/">
+                Voltar ao catálogo
+            </Link>
+        </SuccessContainer>
     )
 }
