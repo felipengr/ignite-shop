0715efaa0078256c3dc9b5d8ce790c52ecc1a295
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-03-15 20:42:11 -0300

    finalizando success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -4,17 +4,18 @@ import Image from "next/image";
 import Link from "next/link";
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
-import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+import {
+    ImageContainer,
+    ImagesContainer,
+    SuccessContainer
+} from "../styles/pages/success";
 
 interface SuccessProps {
     customerName: string
-    product: {
-        name: string
-        imageUrl: string
-    }
+    productsImages: string[]
 }
 
-export default function Product({ customerName, product }: SuccessProps) {
+export default function Product({ customerName, productsImages }: SuccessProps) {
     return (
         <>
             <Head>
@@ -22,19 +23,27 @@ export default function Product({ customerName, product }: SuccessProps) {
                 <meta name="robots" content="noindex" />
             </Head>
             <SuccessContainer>
-            <h1>Compre efetuada!</h1>
-            <ImageContainer>
-                <Image src={product.imageUrl} width={120} height={110} alt="" />
-            </ImageContainer>
-            <p>
-               Uhuul <strong>{customerName}</strong>, sua
-               <strong> {product.name}</strong> já está a caminho 
-               da sua casa.
-            </p>
-            <Link href="/">
-                Voltar ao catálogo
-            </Link>
-        </SuccessContainer>
+                <ImagesContainer>
+                    {productsImages.map((image, i) => (
+                        <ImageContainer key={i}>
+                            <Image
+                                src={image}
+                                width={120}
+                                height={110}
+                                alt=""
+                            />
+                        </ImageContainer>
+                    ))}
+                </ImagesContainer>
+                <h1>Compre efetuada!</h1>
+                <p>
+                Uhuul <strong>{customerName}</strong>, sua compra de {" "}
+                {productsImages.length} camisetas já está a caminho da sua casa.
+                </p>
+                <Link href="/">
+                    Voltar ao catálogo
+                </Link>
+            </SuccessContainer>
         </>
     )
 }
@@ -56,15 +65,15 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
     })
 
     const customerName = session.customer_details.name
-    const product = session.line_items?.data[0].price?.product as Stripe.Product
+    const productsImages = session.line_items.data.map(item => {
+        const product = item.price.product as Stripe.Product
+        return product.images[0]
+    })
 
     return {
         props: {
             customerName,
-            product: {
-                name: product.name,
-                imageUrl: product.images[0],
-            },
+            productsImages,
         }
     }
 }
