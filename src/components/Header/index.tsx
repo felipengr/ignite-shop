ac303ef1edafadaa913a1ce220798954b8a194f8
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-03-15 21:13:23 -0300

    removendo cart button da success

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,19 @@ import Image from 'next/image'
 import { HeaderContainer } from "./styles";
 import logoImg from '../../assets/logo.svg'
 import { Cart } from "../Cart";
+import { useRouter } from "next/router";
 
 export function Header() {
+    const { pathname } = useRouter()
+
+    const showCartButton = pathname !== '/success'
+
     return (
         <HeaderContainer>
             <Link href="/" prefetch={false}>
             <Image src={logoImg} alt="" />
             </Link>
-            <Cart />
+            {showCartButton && <Cart />}
         </HeaderContainer>
     )
 }
